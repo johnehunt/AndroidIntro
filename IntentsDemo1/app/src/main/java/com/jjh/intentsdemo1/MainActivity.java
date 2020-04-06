8efdc0a25d4e20d7a270b4c08a460c23110821ba
John Hunt <jeh@midmarsh.co.uk>
2020-04-06 12:19:49 +0100

    Example of using simple intents to trigger other Android applications

---
+++ IntentsDemo1/app/src/main/java/com/jjh/intentsdemo1/MainActivity.java
@@ -0,0 +1,48 @@
+package com.jjh.intentsdemo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText text1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text1 = findViewById(R.id.editText);
+
+        // Set up the buttons
+        Button dialButton = findViewById(R.id.dialButton);
+        dialButton.setOnClickListener(new DialButtonHandler());
+        Button searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new SearchButtonHandler());
+    }
+
+    private class DialButtonHandler implements View.OnClickListener {
+        public void onClick(View v) {
+            String myData = text1.getText().toString();
+            // Creates an Intent to trigger dialer
+            Intent myActivity2 = new Intent(Intent.ACTION_DIAL, Uri.parse(myData));
+            startActivity(myActivity2);
+        }
+    }
+
+    private class SearchButtonHandler implements View.OnClickListener {
+        public void onClick(View v) {
+            // triggers seaching
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, "MG F Car");
+            startActivity(intent);
+        }
+    }
+
+}

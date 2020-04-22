2cac70bce8cb8f14145df4c0f705843efd135d83
John Hunt <jeh@midmarsh.co.uk>
2020-04-22 19:52:29 +0100

    Simplified example

--- ServiceDemo/app/src/main/java/com/jjh/servicedemo/MainActivity.java
+++ ServiceDemo/app/src/main/java/com/jjh/servicedemo/MainActivity.java
@@ -20,28 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         message = findViewById(R.id.message);
-        Button btnStartService = findViewById(R.id.startService);
-        btnStartService.setOnClickListener(new StartButtonHandler());
-        Button btnStopService = findViewById(R.id.stopService);
-        btnStopService.setOnClickListener(new StopButtonHandler());
         Log.d("<<ServiceDemo1Activity>>", "Ready!");
     }
 
-    class StartButtonHandler implements View.OnClickListener {
-        public void onClick(View v) {
+        public void onStartButtonClick(View v) {
             try {
                 Log.d("<<ServiceDemo1Activity>>", "Starting service!");
-                sampleServiceIntent = new Intent(MainActivity.this, SampleService.class);
+                sampleServiceIntent = new Intent(this, SampleService.class);
                 startService(sampleServiceIntent);
                 message.setText("Started Service");
             } catch (Exception e) {
-                Toast.makeText(getApplicationContext(), e.getMessage(), 1).show();
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
-    }
 
-    class StopButtonHandler implements View.OnClickListener {
-        public void onClick(View v) {
+
+        public void onStopButtonClick(View v) {
             try {
                 Log.d("<<ServiceDemo1Activity>>", "Stopping service!");
                 stopService(sampleServiceIntent);
@@ -50,5 +44,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), e.getMessage(), 1).show();
             }
         }
-    }
+
 }

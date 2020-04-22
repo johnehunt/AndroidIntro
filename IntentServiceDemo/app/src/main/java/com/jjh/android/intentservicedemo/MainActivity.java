37fcf9aede52704d58787111cacb5f6b3e22857e
John Hunt <jeh@midmarsh.co.uk>
2020-04-22 19:48:51 +0100

    Removed button handler

--- IntentServiceDemo/app/src/main/java/com/jjh/android/intentservicedemo/MainActivity.java
+++ IntentServiceDemo/app/src/main/java/com/jjh/android/intentservicedemo/MainActivity.java
@@ -9,21 +9,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private class ButtonHandler implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, LoggerIntentService.class);
-            startService(intent);
-        }
+    public void onClickStartService(View v) {
+        Intent intent = new Intent(this, LoggerIntentService.class);
+        startService(intent);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new ButtonHandler());
     }
 
 }

c919ef94144fcecea50fa30b6a46f506da46b3f6
John Hunt <jeh@midmarsh.co.uk>
2020-04-22 19:46:20 +0100

    Removed handler class

--- IntentDemo3/app/src/main/java/com/jjh/intentdemo3/SubActivity.java
+++ IntentDemo3/app/src/main/java/com/jjh/intentdemo3/SubActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,8 +22,6 @@ public class SubActivity extends AppCompatActivity {
 
         // bind UI variables to Java code
         text = findViewById(R.id.editText);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new ClickButtonHandler());
 
         // Retrieve data sent by parent
         Bundle myBundle = getIntent().getExtras();
@@ -33,11 +29,9 @@ public class SubActivity extends AppCompatActivity {
         data = myBundle.getString("msg");
     }
 
-    private class ClickButtonHandler implements OnClickListener {
-        public void onClick(View v) {
+    public void onCLickSendResponse(View v) {
             finish();
         }
-    }
 
     @Override
     public void finish() {
--- IntentsDemo1/app/src/main/java/com/jjh/intentsdemo1/MainActivity.java
+++ IntentsDemo1/app/src/main/java/com/jjh/intentsdemo1/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,9 +21,6 @@ public class MainActivity extends AppCompatActivity {
         // Initialize dialier text field field
         diallerText = findViewById(R.id.editText);
 
-        // Set up the search button
-        Button searchButton = findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(new SearchButtonHandler());
     }
 
     // Handler method for DIal Button
@@ -35,14 +31,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    // Inner class iused to handle search button
-    class SearchButtonHandler implements View.OnClickListener {
-        public void onClick(View v) {
+    // Inner class used to handle search button
+    public void onSearchButtonClick(View v) {
             // triggers web search activity
             Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
             intent.putExtra(SearchManager.QUERY, "MG F Car");
             startActivity(intent);
-        }
     }
 
 }

78d9f6f6d52e956a9be1fcf4909326c203dfffb1
John Hunt <jeh@midmarsh.co.uk>
2020-04-23 09:25:05 +0100

    Updates

--- DynamicFragmentDemo/app/src/main/java/com/jjh/android/dynamicfragmentdemo/MainActivity.java
+++ DynamicFragmentDemo/app/src/main/java/com/jjh/android/dynamicfragmentdemo/MainActivity.java
@@ -13,48 +13,28 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MyActivity";
 
-    class FragmentOneButtonHandler implements View.OnClickListener {
-
-        private static final String FRAG_TAG = "Fragment1ButtonHandler";
-
-        @Override
-        public void onClick(View v) {
-            Log.d(FRAG_TAG , "onClick()");
-            FragmentOne fragment = new FragmentOne();
-            FragmentManager manager = getSupportFragmentManager();
-            FragmentTransaction transaction = manager.beginTransaction();
-            transaction.replace(R.id.displaylayout, fragment);
-            transaction.commit();
-        }
+    public void onFragmentOneButtonClick(View v) {
+        Log.d(TAG, "onClick()");
+        FragmentOne fragment = new FragmentOne();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.displaylayout, fragment);
+        transaction.commit();
     }
 
-    class FragmentTwoButtonHandler implements View.OnClickListener {
-
-        private static final String FRAG_TAG = "Fragment2ButtonHandler";
-
-        @Override
-        public void onClick(View v) {
-            Log.d(FRAG_TAG , "onClick()");
-            FragmentTwo fragment = new FragmentTwo();
-            FragmentManager manager = getSupportFragmentManager();
-            FragmentTransaction transaction = manager.beginTransaction();
-            transaction.replace(R.id.displaylayout, fragment);
-            transaction.commit();
-        }
+    public void onFragmentTwoButtonClick(View v) {
+        Log.d(TAG, "onClick()");
+        FragmentTwo fragment = new FragmentTwo();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.displaylayout, fragment);
+        transaction.commit();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_main);
-
-        Button showFragmentOneButton = findViewById(R.id.button1);
-        showFragmentOneButton.setOnClickListener(new FragmentOneButtonHandler());
-
-        Button showFragmentTwoButton = findViewById(R.id.button2);
-        showFragmentTwoButton.setOnClickListener(new FragmentTwoButtonHandler());
-
     }
 }

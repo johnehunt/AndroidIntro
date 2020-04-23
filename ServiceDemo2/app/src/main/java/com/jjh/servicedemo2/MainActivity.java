1efeff225a73bea3b71fd0d157796ac6b4e2d541
John Hunt <jeh@midmarsh.co.uk>
2020-04-23 09:22:32 +0100

    Tidied layout

--- ServiceDemo2/app/src/main/java/com/jjh/servicedemo2/MainActivity.java
+++ ServiceDemo2/app/src/main/java/com/jjh/servicedemo2/MainActivity.java
@@ -1,15 +1,14 @@
 package com.jjh.servicedemo2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 

260bcadb47e3a46654b2e1bb8e97e217c8e55f5f
John Hunt <jeh@midmarsh.co.uk>
2020-04-23 09:25:21 +0100

    Removed unused import

--- DynamicFragmentDemo/app/src/main/java/com/jjh/android/dynamicfragmentdemo/MainActivity.java
+++ DynamicFragmentDemo/app/src/main/java/com/jjh/android/dynamicfragmentdemo/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 

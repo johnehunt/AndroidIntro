a9fc80664a91518c60f5e3cdd8db0a687f78ec39
John Hunt <jeh@midmarsh.co.uk>
2020-04-30 10:02:34 +0100

    Added tag for logging

--- ServiceDemo/app/src/main/java/com/jjh/servicedemo/SampleService.java
+++ ServiceDemo/app/src/main/java/com/jjh/servicedemo/SampleService.java
@@ -7,36 +7,26 @@ import android.util.Log;
 
 public class SampleService extends Service {
 
+    private static final String TAG = "SampleService";
+
     public IBinder onBind(Intent intent) {
-        Log.d("SampleService", "onBind()");
+        Log.d(TAG, "onBind()");
         return null;
     }
 
     public void onCreate() {
         super.onCreate();
-        Log.d("SampleService", "onCreate()");
+        Log.d(TAG, "onCreate()");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("SampleService", "onStartCommand()");
+        Log.d(TAG, "onStartCommand()");
         return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public boolean onUnbind(Intent intent) {
-        Log.d("SampleService", "onUnbind()");
-        return super.onUnbind(intent);
-    }
-
-    @Override
-    public void onRebind(Intent intent) {
-        super.onRebind(intent);
-        Log.d("SampleService", "onRebind()");
     }
 
     public void onDestroy() {
         super.onDestroy();
-        Log.d("SampleService", "onDestroy()");
+        Log.d(TAG, "onDestroy()");
     }
 }
